98321c39b4c01040951d7c51d66935c1ae4c4ee1
Ghost Rider <dianamulungi@gmail.com>
2018-11-19 11:06:31 +0300

    manage shell added

---
+++ manage.py
@@ -0,0 +1,32 @@
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User
+from  flask_migrate import Migrate, MigrateCommand
+# from flask_script import Manager,Server
+
+# Creating app instance
+
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+# manager = Manager(app)
+# manager.add_command('server',Server)
+
+# def test():
+#     """Run the unit tests."""
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    manager.run()
